276b944c5c685f889a4b9111458f82a10b904a91
Chris Colborne <zorfling@gmail.com>
2019-01-03 16:57:50 +1000

    Initial crack - grab from API, show cards direct in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'flight-tracker';
+
+  flights = [
+    {
+      callsign: 'Flight 1',
+      arrival: '12:00:00'
+    },
+    {
+      callsign: 'Flight 2',
+      arrival: '13:00:00'
+    },
+    {
+      callsign: 'Flight 3',
+      arrival: '14:00:00'
+    }
+  ];
+
+  constructor() {}
+
+  getData() {
+    let headers = new Headers();
+    const username = 'zorfling';
+    const password = 'opensky';
+    headers.append('Authorization', 'Basic ' + btoa(username + ':' + password));
+
+    const begin = Math.floor(
+      (new Date().getTime() - 60 * 60 * 8 * 1000) / 1000
+    );
+    const end = Math.floor((new Date().getTime() + 60 * 60 * 1 * 1000) / 1000);
+
+    const data = fetch(
+      `https://opensky-network.org/api/flights/arrival?airport=YBBN&begin=${begin}&end=${end}`,
+      { headers }
+    )
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        return myJson.map(elem => ({
+          callsign: elem.callsign,
+          lastSeen: new Date(elem.lastSeen * 1000),
+          from: elem.estDepartureAirport
+        }));
+      });
+
+    return data;
+  }
+
+  async ngOnInit() {
+    this.flights = await this.getData();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
